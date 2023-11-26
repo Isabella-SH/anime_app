041860b7b448563c37ec9e5fc883b361030b08e4
Isabella <sorianoisabella28@gmail.com>
2023-11-26 01:34:41 -0500

    mostrar los anime_preferences.dart usado anime_item

--- lib/screens/anime_preferences.dart
+++ lib/screens/anime_preferences.dart
@@ -1,5 +1,6 @@
 import 'package:anime_app/model/anime.dart';
 import 'package:anime_app/repositories/anime_repository.dart';
+import 'package:anime_app/screens/anime_list.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -34,36 +35,8 @@ class _AnimePreferencesState extends State<AnimePreferences> {
     return ListView.builder(
       itemCount: _animes?.length ?? 0,
       itemBuilder: ((context, index) {
-        final anime = _animes?[index];
-
-        return Card(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // Muestra la imagen si está disponible
-                if (anime?.images?.jpg?.imageUrl != null)
-                  Image.network(
-                    anime!.images!.jpg!.imageUrl!,
-                    height: 100, // Ajusta según sea necesario
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  ),
-
-                // Muestra el título del anime
-                Text(
-                  anime?.title ?? '',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16,
-                  ),
-                ),
-
-              ],
-            ),
-          ),
-        );
+        final anime = _animes![index];
+        return AnimeItem(anime: anime);
       }),
     );
   }
